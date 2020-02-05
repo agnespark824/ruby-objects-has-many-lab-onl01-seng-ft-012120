dc6feb8687cc4d437046fd5ab885a8a8ab55bcac
Agnes Park <agnespark824@gmail.com>
2020-02-05 08:48:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,22 @@
 class Post
-  attr_accessor
+  attr_accessor :author :title
   
-  def initialize
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author
     
   end
   
+  def author_name
+  
+  end
 end
