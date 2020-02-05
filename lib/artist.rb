4570b458b8511499bce7dd0c0522f71246480129
Agnes Park <agnespark824@gmail.com>
2020-02-05 08:09:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,15 +3,18 @@ class Artist
   
   def initialize(name)
     @name = name
-    #@@songs
+    @songs = []
   end
   
   def add_song(song)
     song.artist = self
+    @songs << song
   end
   
-  def add_song_by_name(song)
-    song = Song.new
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+    @songs << song
   end
   
 end
